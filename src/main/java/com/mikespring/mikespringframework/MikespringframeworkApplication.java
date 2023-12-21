077dbafd1bd743d4423b57eb8908f334bd666e8c
mik284 <mikeykamau222@gmail.com>
2023-12-21 12:29:25 +0300

    initial endpoint commit  hello mike

---
+++ src/main/java/com/mikespring/mikespringframework/MikespringframeworkApplication.java
@@ -0,0 +1,13 @@
+package com.mikespring.mikespringframework;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikespringframeworkApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MikespringframeworkApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mikespring/mikespringframework/MikespringframeworkApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mikespring.mikespringframework;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MikespringframeworkApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
