e2676d044b76a7cd4690e39068289c10955e119b
mik284 <mikeykamau222@gmail.com>
2023-12-22 15:23:13 +0300

    developed a RESTful web service with Spring

--- src/main/java/com/mikespring/mikespringframework/HelloWorldController.java
+++ src/main/java/com/mikespring/mikespringframework/HelloWorldController.java
@@ -1,14 +1,24 @@
 package com.mikespring.mikespringframework;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.concurrent.atomic.AtomicLong;
 
 @RestController
 public class HelloWorldController {
+
+    private static final String template = "Hello, %s!";
+    private final AtomicLong counter = new AtomicLong();
+    public record Greeting(long id, String content) { }
 
     @GetMapping(path = "/hello")
     public String helloWorld(){
         return "Hello mike!, whatsapp";
     }
-
+    @GetMapping("/greeting")
+    public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+        return new Greeting(counter.incrementAndGet(), String.format(template, name));
+    }
 }
