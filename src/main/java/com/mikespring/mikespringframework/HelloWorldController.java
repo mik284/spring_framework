24dbb0a4bbb0f72da590c8b3c24c0bf7ec937ecd
mik284 <mikeykamau222@gmail.com>
2023-12-22 15:32:55 +0300

    added cors dep & annotation

--- src/main/java/com/mikespring/mikespringframework/HelloWorldController.java
+++ src/main/java/com/mikespring/mikespringframework/HelloWorldController.java
@@ -1,11 +1,13 @@
 package com.mikespring.mikespringframework;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.atomic.AtomicLong;
 
+@CrossOrigin
 @RestController
 public class HelloWorldController {
 
